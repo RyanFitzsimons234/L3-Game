29a8c81a837bd684573fadb1df96338af6925ab0
RyanFitzsimons234 <80649767+RyanFitzsimons234@users.noreply.github.com>
2023-08-15 22:26:18 +1200

    Add files via upload
    made canvas scale with screen size

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+const GameViewport = {
+    WIDTH: 384,
+    HEIGHT: 224,
+    SCALE: 4,
+}
+
 window.onload = function() {
-    console.log("java is working");
+    const canvasEL = document.querySelector('canvas');
+    const context = canvasEL.getContext('2d');
+
+    canvasEL.width = GameViewport.WIDTH;
+    canvasEL.height = GameViewport.HEIGHT;
+
+    canvasEL.style.width = `${GameViewport.WIDTH * GameViewport.SCALE}px`;
+    canvasEL.style.height = `${GameViewport.HEIGHT * GameViewport.SCALE}px`;
+
+    console.log(context);
 }
