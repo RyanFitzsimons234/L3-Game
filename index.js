c141028e2789c20a693b7af35c2311e39dbf12e0
RyanFitzsimons234 <80649767+RyanFitzsimons234@users.noreply.github.com>
2023-08-22 17:08:11 +1200

    Add files via upload
    added sprite animation

--- index.js
+++ index.js
@@ -1,49 +1,50 @@
+import { Ken } from './entities/fighters/Ken.js';
+import { Ryu } from './entities/fighters/Ryu.js';
+import { Stage } from './entities/Stage.js';
+import { FpsCounter } from './entities/FpsCounter.js';
+import { STAGE_FLOOR } from './constants/stage.js';
+
+
 const GameViewport = {
     WIDTH: 384,
     HEIGHT: 220,
-    SCALE: 4,
 }
 
-window.onload = function () {
+window.addEventListener  ('load', function() {
     const canvasEL = document.querySelector('canvas');
     const context = canvasEL.getContext('2d');
 
     canvasEL.width = GameViewport.WIDTH;
     canvasEL.height = GameViewport.HEIGHT;
 
-    const [bob, background] = document.querySelectorAll('img');
+    const entities = [
+    new Stage(),
+    new Ken(80, STAGE_FLOOR, 100),
+    new Ryu(80, STAGE_FLOOR, -100),
+    new FpsCounter(),
+    ];
 
-    const position = {
-        x: GameViewport.WIDTH / 2 - bob.width / 2,
-        y: 110,
-    };
-
-    let velocity = 1;
+    let frameTime = {
+        previous: 0,
+        secondsPassed: 0,
+    }
 
 
+    function frame(time) {
+        window.requestAnimationFrame(frame);
 
-    function frame() {
-        position.x += velocity;
-
-        if (position.x > GameViewport.WIDTH - bob.width || position.x < 0) {
-            velocity = -velocity;
+        frameTime = {
+        secondsPassed: (time - frameTime.previous) / 1000,
+        previous: time,
+        }
+        for(const entity of entities) {
+            entity.update(frameTime, context);
         }
 
-        context.drawImage(background, 0, 0);
-
-        // context.strokeStyle = 'yellow';
-        // context.moveTo(0, 0);
-        // context.lineTo(GameViewport.WIDTH, GameViewport.HEIGHT);
-        // context.moveTo(GameViewport.WIDTH, 0);
-        // context.lineTo(0, GameViewport.HEIGHT);
-        // context.stroke();
-    
-        context.drawImage(bob, position.x, position.y);
-
-        window.requestAnimationFrame(frame);
+        for(const entity of entities) {
+            entity.draw(context);
+        }
     }
 
     window.requestAnimationFrame(frame);
-
-
-}
+});
