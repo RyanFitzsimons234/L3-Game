bb80d30ecdca5086f8c4408d32371555094ddc53
RyanFitzsimons234 <80649767+RyanFitzsimons234@users.noreply.github.com>
2023-08-24 11:46:58 +1200

    Add files via upload
    fixed the form that changes states

--- index.js
+++ index.js
@@ -3,18 +3,51 @@ import { Ryu } from './entities/fighters/Ryu.js';
 import { Stage } from './entities/Stage.js';
 import { FpsCounter } from './entities/FpsCounter.js';
 import { STAGE_FLOOR } from './constants/stage.js';
-import { FighterDirection } from './constants/fighter.js';
+import { FighterDirection, FighterState } from './constants/fighter.js';
 
-window.addEventListener  ('load', function() {
+function populateMoveDropdown(state) {
+    const dropdown = document.getElementById('state-dropdown');
+
+    Object.entries(FighterState).forEach(([, value]) => {
+        const option = document.createElement('option');
+        option.setAttribute('value', value);
+        option.innerText = value;
+        dropdown.appendChild (option);
+    });
+}
+
+function handleFormSubmit(event, fighters) {
+    event.preventDefault();
+
+    const selectedCheckboxes = Array
+    .from(event.target.querySelectorAll('input:checked'))
+    .map(checkbox => checkbox.value);
+
+    const options = event.target.querySelector('select');
+
+    fighters.forEach(fighter => {
+        if (selectedCheckboxes.includes(fighter.name)) {
+            fighter.changeState(options.value)
+        }
+    });
+}
+
+window.addEventListener('load', function() {
+    populateMoveDropdown();
+
     const canvasEL = document.querySelector('canvas');
     const context = canvasEL.getContext('2d');
 
     context.imageSmoothingEnabled = false;
 
+    const fighters = [
+        new Ryu(104, STAGE_FLOOR, FighterDirection.RIGHT),
+        new Ken(280, STAGE_FLOOR, FighterDirection.LEFT),
+    ]
+
     const entities = [
     new Stage(),
-    new Ryu(104, STAGE_FLOOR, FighterDirection.RIGHT),
-    new Ken(280, STAGE_FLOOR, FighterDirection.LEFT),
+    ...fighters,
     new FpsCounter(),
     ];
 
@@ -39,6 +72,8 @@ window.addEventListener  ('load', function() {
             entity.draw(context);
         }
     }
+ 
+    this.document.addEventListener('submit', (event) => handleFormSubmit(event, fighters));
 
     window.requestAnimationFrame(frame);
 });
