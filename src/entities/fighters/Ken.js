b65383478c24726d5ec79371f382398e545a50d8
RyanFitzsimons234 <80649767+RyanFitzsimons234@users.noreply.github.com>
2023-08-23 16:28:58 +1200

    Add files via upload
    set up state machine

--- src/entities/fighters/Ken.js
+++ src/entities/fighters/Ken.js
@@ -1,6 +1,5 @@
-import {
-    Fighter
-} from "./Fighter.js";
+import { FighterState } from '../../constants/fighter.js';
+import { Fighter } from "./Fighter.js";
 
 export class Ken extends Fighter {
     constructor(x, y, velocity) {
@@ -26,8 +25,8 @@ export class Ken extends Fighter {
             ['backwards-6', [[773, 866, 57, 89], [36, 87]]],            
         ]);
         this.animations = {
-            'walkForwards' : ['forwards-1', 'forwards-2', 'forwards-3', 'forwards-4', 'forwards-5', 'forwards-6'],
-            'walkBackwards' : ['backwards-1', 'backwards-2', 'backwards-3', 'backwards-4', 'backwards-5', 'backwards-6'],
+            [FighterState.WALK_FORWARD] : ['forwards-1', 'forwards-2', 'forwards-3', 'forwards-4', 'forwards-5', 'forwards-6'],
+            [FighterState.WALK_BACKWARD] : ['backwards-1', 'backwards-2', 'backwards-3', 'backwards-4', 'backwards-5', 'backwards-6'],
         }
     }
 }
