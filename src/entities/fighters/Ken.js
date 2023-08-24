97501d1d7f79a33c88fd1a46d0af671c5fb17067
RyanFitzsimons234 <80649767+RyanFitzsimons234@users.noreply.github.com>
2023-08-25 00:49:17 +1200

    Add files via upload
    added a jump state

--- src/entities/fighters/Ken.js
+++ src/entities/fighters/Ken.js
@@ -8,6 +8,12 @@ export class Ken extends Fighter {
         this.image = document.querySelector('img[alt="ken"]');
 
         this.frames = new Map ([
+            // idle Stance
+            ['idle-1', [[346, 688, 60, 89], [36, 86]]],
+            ['idle-2', [[2, 687, 59, 90], [33, 87]]],
+            ['idle-3', [[72, 685, 58, 92], [32, 89]]],
+            ['idle-4', [[142, 684, 55, 93], [31, 98]]],
+
             // Moves Forwards
             ['forwards-1', [[8, 872, 53, 83], [27, 81]]],
             ['forwards-2', [[70, 867, 60, 88], [35, 86]]],
@@ -22,11 +28,28 @@ export class Ken extends Fighter {
             ['backwards-3', [[558, 865, 57, 90], [36, 88]]],
             ['backwards-4', [[629, 864, 58, 90], [38, 89]]],
             ['backwards-5', [[702, 865, 58, 91], [36, 88]]],
-            ['backwards-6', [[773, 866, 57, 89], [36, 87]]],            
+            ['backwards-6', [[773, 866, 57, 89], [36, 87]]],
+            
+            // Moves Backwards
+            ['jump-up-1', [[724, 1036, 56, 104], [32, 107]]],
+            ['jump-up-2', [[792, 995, 50, 89], [25, 103]]],
+            ['jump-up-3', [[853, 967, 54, 77], [25, 103]]],
+            ['jump-up-4', [[911, 966, 48, 70], [28, 101]]],
+            ['jump-up-5', [[975, 977, 48, 86], [25, 103]]],
+            ['jump-up-6', [[1031, 1008, 55, 103], [32, 107]]],
         ]);
+
         this.animations = {
+            [FighterState.IDLE] : ['idle-1', 'idle-2', 'idle-3', 'idle-4', 'idle-3', 'idle-2'],
             [FighterState.WALK_FORWARD] : ['forwards-1', 'forwards-2', 'forwards-3', 'forwards-4', 'forwards-5', 'forwards-6'],
             [FighterState.WALK_BACKWARD] : ['backwards-1', 'backwards-2', 'backwards-3', 'backwards-4', 'backwards-5', 'backwards-6'],
-        }
+            [FighterState.JUMP_UP] : ['jump-up-1',],
+        };
+
+        this.initialVelocity = {
+            jump: -420,
+        };
+
+        this.gravity = 1000;
     }
 }
