21efa151e075170d4ce66d7b6d37770ccde08377
RyanFitzsimons234 <80649767+RyanFitzsimons234@users.noreply.github.com>
2023-08-26 11:48:30 +1200

    Add files via upload
    added a crouching state and refactored index file

--- src/constants/fighter.js
+++ src/constants/fighter.js
@@ -10,4 +10,7 @@ export const FighterState = {
     JUMP_UP: 'jumpUp',
     JUMP_FORWARD: 'jumpForwards',
     JUMP_BAKCWARD: 'jumpBackwards',
+    CROUCH: 'crouch',
+    CROUCH_DOWN: 'crouchDown',
+    CROUCH_UP: 'crouchUp',
 };
