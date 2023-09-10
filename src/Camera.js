3a813db0bced26395618ae2645cfa1c5062bd3c2
RyanFitzsimons234 <80649767+RyanFitzsimons234@users.noreply.github.com>
2023-09-10 20:36:42 +1200

    Add files via upload
    added horizontal scrolling

--- src/Camera.js
+++ src/Camera.js
@@ -1,4 +1,4 @@
-import { SCROLL_BOUNDARY } from "./constants/stage";
+import { SCROLL_BOUNDARY, STAGE_HEIGHT, STAGE_WIDTH, STAGE_PADDING } from "./constants/stage.js";
 
 export class Camera {
     constructor(x, y, fighters) {
@@ -14,6 +14,26 @@ export class Camera {
 
         if (highX - lowX > context.canvas.width - SCROLL_BOUNDARY * 2) {
             const midPoint = (highX - lowX) / 2;
+            this.position.x = lowX + midPoint - (context.canvas.width / 2);
+        } else {
+            for (const fighter of this.fighters) {
+                if (fighter.position.x < this.position.x + SCROLL_BOUNDARY
+                    && fighter.velocity.x * fighter.direction < 0
+                    || fighter.position.x > this.position.x + context.canvas.width - SCROLL_BOUNDARY
+                    && fighter.velocity.x * fighter.direction > 0
+                ) {
+                    this.position.x += fighter.velocity.x * fighter.direction * time.secondsPassed;
+                }
+            }
+        }
+
+        if (this.position.x < STAGE_PADDING) this.position.x = STAGE_PADDING;
+        if (this.position.x > STAGE_WIDTH + STAGE_PADDING - context.canvas.width) {
+            this.position.x = STAGE_WIDTH + STAGE_PADDING - context.canvas.width;
+        }
+        if (this.position.y < 0) this.position.y = 0;
+        if (this.position.y > STAGE_HEIGHT - context.canvas.height) {
+            this.position.y = STAGE_HEIGHT - context.canvas.height;
         }
     }
 }
