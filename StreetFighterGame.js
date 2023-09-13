13a06ed827393a381c18e2f71e8bbfce2a4a7a23
RyanFitzsimons234 <80649767+RyanFitzsimons234@users.noreply.github.com>
2023-09-13 16:28:55 +1200

    Add files via upload
    added background objects and some refactoring

--- StreetFighterGame.js
+++ StreetFighterGame.js
@@ -1,49 +1,63 @@
 import { Ken } from './entities/fighters/Ken.js';
 import { Ryu } from './entities/fighters/Ryu.js';
-import { Stage } from './entities/stage/Stage.js';
-import { FpsCounter } from './entities/FpsCounter.js';
+import { KenStage } from './entities/stage/KenStage.js';
+import { FpsCounter } from './entities/overlays/FpsCounter.js';
 import { STAGE_MID_POINT, STAGE_PADDING } from './constants/stage.js';
-import { pullGamepads, registerGamepadEvents, registerKeyboardEvents } from './InputHandler.js';
+import { pullGamepads, registerGamepadEvents, registerKeyboardEvents } from './engine/InputHandler.js';
 import { Shadow } from './entities/fighters/Shadow.js';
 import { StatusBar } from './entities/overlays/StatusBar.js';
-import { Camera } from './Camera.js';
+import { Camera } from './engine/Camera.js';
 import { getContext } from './utils/context.js';
 
 export class StreetFighterGame {
-    constructor() {
-        this.context = getContext();
-        this.fighters = [new Ryu(1), new Ken(0)];
+        context = getContext();
+        fighters = [new Ryu(1), new Ken(0)];
+        camera = new Camera(STAGE_MID_POINT + STAGE_PADDING - (this.context.canvas.width / 2), 16, this.fighters);
+
+        frameTime = {
+            previous: 0,
+            secondsPassed: 0,
+        };
+
+        constructor() {
+        this.stage = new KenStage();
 
         this.fighters[0].opponent = this.fighters[1];
         this.fighters[1].opponent = this.fighters[0];
 
-        this.camera = new Camera(STAGE_MID_POINT + STAGE_PADDING - (this.context.canvas.width / 2), 16, this.fighters);
-
         this.entities = [
-            new Stage(),
             ...this.fighters.map(fighter => new Shadow(fighter)),
             ...this.fighters,
-            new FpsCounter(),
-            new StatusBar(this.fighters),
         ];
 
-        this.frameTime = {
-            previous: 0,
-            secondsPassed: 0,
-        }
+        this.overlays = [
+            new StatusBar(this.fighters),
+            new FpsCounter(),
+        ];
     }
+
 
     update() {
         this.camera.update(this.frameTime, this.context);
+        this.stage.update(this.frameTime, this.context);
 
         for (const entity of this.entities) {
             entity.update(this.frameTime, this.context, this.camera);
         }
+
+        for (const overlay of this.overlays) {
+            overlay.update(this.frameTime, this.context, this.camera);
+        }
     }
 
     draw() {
+        this.stage.drawBackground(this.context, this.camera);
         for (const entity of this.entities) {
             entity.draw(this.context, this.camera);
+        }
+        this.stage.drawForeground(this.context, this.camera);
+        for (const overlay of this.overlays) {
+            overlay.draw(this.context, this.camera);
         }
     }
 
